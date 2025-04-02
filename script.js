ac1c52b541b233291795da7f90524ac016da6b87
Nishath-Mumtahin <mumtahinnishath@gmail.com>
2025-04-02 21:17:54 +0600

    added color bg js

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ document.addEventListener("NavEndButton", ()=>{
 const colorButton = document.querySelector(".navbar-end button:nth-child(2)");
 colorButton.addEventListener("click", ()=>{
     document.body.style.backgroundColor = getRandomColor();
-})
-})
+});
+const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#"
+    for(let i = 0; i<6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+
+
+
+
+});
